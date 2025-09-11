c49a41747be42d893a9d49c5941d1f3596ec8f14
nam nguyen <nguyenvuhainamhan@gmail.com>
2025-09-11 16:08:57 -0700

    init

---
+++ app/src/main/java/sfu/namnguyen/myruns/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package sfu.namnguyen.myruns
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+    }
+}
